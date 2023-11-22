5e83d3db52f7a19093246732dc73f6fc7af4e759
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-22 14:53:10 -0500

    Added login and home page to router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.jsx'
-import './index.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
