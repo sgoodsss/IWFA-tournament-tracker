3592c075b5e127e9c1773f3b171b1644677da008
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-22 14:30:38 -0500

    Created basic structure for new project

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } 
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
