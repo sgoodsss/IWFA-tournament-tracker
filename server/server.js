bcbaebd581b9cbc21152fcc965f62ef634e667f4
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-27 20:43:30 -0500

    Added back in commented out code

--- server/server.js
+++ server/server.js
@@ -17,9 +17,9 @@ const app = express();
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 
-    // app.get('*', (req, res) => {
-    //   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    // });
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
   } 
 
   db.once('open', () => {
