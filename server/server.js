21614d5a6c271c3808ba8e8e2a37689c683f7144
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-27 20:49:19 -0500

    Changed client/dist

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
