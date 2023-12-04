b5151f1a20817dbd1fa00d4dd27b59edefdfb68a
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-12-04 17:32:41 -0500

    Changed LoginForm and SignUpForm from js to jsx files

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,7 +1,10 @@
+import LoginForm from '../components/LoginForm.jsx';
+
 function Login () {
     return (
         <div>
             <h1>Login</h1>
+            <LoginForm />
         </div>
     )
     
