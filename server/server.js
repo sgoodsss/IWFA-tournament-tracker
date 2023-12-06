81f37db18d3ac0096788cb74f9ed64dcdc08fa35
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-12-06 13:50:05 -0500

    Changed both builds to dist in server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
