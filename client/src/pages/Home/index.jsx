a2c34e9c8f98b1dfe70fc130829b7da283bfe176
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-12-09 14:34:50 -0500

    Added styling to landing page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,26 +1,38 @@
 import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import "./Home.css"
 
-function Home () {
+function Home() {
 
     return (
         <div>
-            <h1>Welcome!</h1>
-            <h2> What would you like to do?</h2>
-            <Button>
-                <Link to={"/login"} style={{"textDecoration": "none", "color" : "white"}}>
-                    Login
-                </Link>
-             </Button>
+            <div className='background-image'>
+                <h1>Welcome Anglers!</h1>
+                <h2>
+                    {" "}
+                    What would you like to do?
+                </h2>
 
-             <Button>
-                <Link to={"/signup"} style={{"textDecoration": "none", "color" : "white"}}>
-                    Sign Up
-                </Link>
-             </Button>
+                <Button id="btn">
+                    <Link to={"/login"} style={{ "textDecoration": "none", "color": "white" }}>
+                        Login
+                    </Link>
+                </Button>
+
+                <Button id="btn" className='btn1'>
+                    <Link to={"/signup"} style={{ "textDecoration": "none", "color": "white" }}>
+                        Sign Up
+                    </Link>
+                </Button>
+
+                <footer id="footer">
+                    Photo by <a href="https://unsplash.com/@sandbarproductions?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Chase Baker</a> on <a href="https://unsplash.com/photos/white-and-blue-boat-on-sea-during-daytime-yvb8NrIhIFI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                </footer>
+
+            </div>
         </div>
     )
-    
+
 }
 
 export default Home;
