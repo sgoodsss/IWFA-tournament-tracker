b7b06275abd0b7d4cf58ad41f7d4b0760c71ee9f
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-12-11 11:06:55 -0500

    Began creating cards for user form data to show on User Home page

--- client/src/pages/User/index.jsx
+++ client/src/pages/User/index.jsx
@@ -1,20 +1,62 @@
 import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import axios from 'axios'
+import { getMe } from '../../utils/API';
+import Auth from '../../utils/auth';
 
 function User() {
-    const [users, setUsers] = useState([])
-    useEffect(() => {
-        axios.get('http://localhost:3001/getUserForms')
-            .then(users => setUsers(users.data))
-            .catch(err => console.log(err))
-    }, [])
+ // Set state variables
+ const [userData, setUserData] = useState('')
+ useEffect(() => {
+     const getUserData = async () => {
+         try {
+             const token = Auth.loggedIn() ? Auth.getToken() : null;
 
+             if (!token) {
+                 return false;
+             }
+
+             const response = await getMe(token);
+
+             if (!response.ok) {
+                 throw new Error('something went wrong!');
+             }
+
+             const user = await response.json();
+             setUserData(user);
+         } catch (err) {
+             console.error(err);
+         }
+     };
+
+     getUserData();
+ });
+    // console.log(userData.day1Entry)
     return (
         <div>
             <h1>User Home</h1>
             <h2>View Previous Scoresheets</h2>
+
+            {/* <div>
+                <h3>Day 1</h3>
+                {userData.day1Entry.map(
+                    day1 => (<h4>{day1.dailyTotal}</h4>)
+                )}
+            </div>
+
+            <div>
+                <h3>Day 2</h3>
+                {userData.day2Entry.map(
+                    day2 => (<h4>{day2.dailyTotal}</h4>)
+                )}
+            </div>
+
+            <div>
+                <h3>Day 3</h3>
+                {userData.day3Entry.map(
+                    day3 => (<h4>{day3.dailyTotal}</h4>)
+                )}
+            </div> */}
 
             <Button>
                 <Link to={"/form"} style={{ "textDecoration": "none", "color": "white" }}>
