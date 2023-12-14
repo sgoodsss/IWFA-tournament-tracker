ec15aa38cbc4420fe5c41453f97b4cf762308281
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-12-14 12:05:33 -0500

    Changed port #

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
 // route to get logged in user's info (needs the token)
 export const getMe = (token) => {
-  return fetch('http://localhost:3001/api/users/me', {
+  return fetch('http://localhost:3000/api/users/me', {
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
@@ -9,7 +9,7 @@ export const getMe = (token) => {
 };
 
 export const createUser = (userData) => {
-  return fetch('http://localhost:3001/api/users', {
+  return fetch('http://localhost:3000/api/users', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -19,7 +19,7 @@ export const createUser = (userData) => {
 };
 
 export const loginUser = (userData) => {
-  return fetch('http://localhost:3001/api/users/login', {
+  return fetch('http://localhost:3000/api/users/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -30,7 +30,7 @@ export const loginUser = (userData) => {
 
 // save form data for a logged in user
 export const saveForm = (formData, token) => {
-  return fetch('http://localhost:3001/api/forms', {
+  return fetch('http://localhost:3000/api/forms', {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
